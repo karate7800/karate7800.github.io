15ae15e4763f4b7bf78cfa958bf2558274cfbcf0
karate7800 <36754494+karate7800@users.noreply.github.com>
2022-01-05 13:17:24 -0500

    initial cookie update

--- kmmc/testthing/main.js
+++ kmmc/testthing/main.js
@@ -1,4 +1,28 @@
 const questions = 1;
+const maxtime = 300;
 
-document.cookie = "alpha = zero;"
-console.log("done")
+var Time = new Date()
+
+if(!document.cookie.contains(ans)) {
+    var lets = ''
+    for(var i=0;i<questions;i++) {
+        lets+='E'
+    }
+    document.cookie = 'ans ='+lets+';time='+Time.getTime();
+
+}
+
+
+//to set a cookie "alpha"
+    // document.cookie = "alpha = zero;"
+//to retreive a cookie "alpha"
+// document.cookie.split(';').find(row => row.startsWith('alpha=')).split('=')[1]
+
+
+//to update with the proper value
+function selOption(selected) {
+    let qNum = selected[1]
+    let ans = document.cookie.split(';').find(row => row.startsWith('ans=').split('=')[1])
+    ans[qNum - 1] = selected[2] 
+}
+
