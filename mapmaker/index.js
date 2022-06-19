69d1f2be9766910f0f53976f602343c42ba08b8a
karate7800 <36754494+karate7800@users.noreply.github.com>
2022-06-19 07:51:03 -0700

    output code properly

--- mapmaker/index.js
+++ mapmaker/index.js
@@ -1,11 +1,13 @@
 let form = document.getElementById("photo")
 let but = document.getElementById("psub")
+const h1 =document.getElementsByTagName('h1')[0]
 const canv = document.getElementById('canvas')
 const ctx = canv.getContext('2d')
 
   const sub = function () {
     let fDate = new FormData(form)
     let rp = fDate['imgpro']
+    h1.innerText = (rp)
     ctx.drawImage(rp, 40, 40)
 }
 
