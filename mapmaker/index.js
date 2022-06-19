4008dcb7424a2bbd96edde6c1917e43f26c34438
karate7800 <36754494+karate7800@users.noreply.github.com>
2022-06-19 07:19:25 -0700

    write form retrieval code

---
+++ mapmaker/index.js
@@ -0,0 +1,13 @@
+let form = document.getElementById("photo")
+let but = document.getElementById("psub")
+const canv = document.getElementById('canvas')
+const ctx = canv.getContext('2d')
+
+  const sub = function () {
+    let fDate = new FormData(form)
+    let rp = fDate['imgpro']
+}
+
+
+
+
