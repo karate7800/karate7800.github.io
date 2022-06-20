80142e85acda1adca252ae682603d61b2a57d82d
karate7800 <36754494+karate7800@users.noreply.github.com>
2022-06-20 10:03:42 -0700

    add point create system

--- mapmaker/index.js
+++ mapmaker/index.js
@@ -3,11 +3,34 @@ let form = document.getElementById("photo")
 let but = document.getElementById("psub")
 const inp = document.getElementById('shit')
 const h1 =document.getElementById('h1')
+const mvbut = document.getElementById('mvbut')
+const plbut = document.getElementById('plbut')
+const pList = document.getElementById('plist')
+
 const canv = document.getElementById('canvas')
 const ctx = canv.getContext('2d')
- 
+let mStatus = null
+
+
+let canvleft = canv.offsetLeft + canv.clientLeft
+let canvtop = canv.offsetTop + canv.clientTop
+
+let points = []
+canv.addEventListener('click', function(e) {
+  let x = e.pageX - canvleft,
+  y = e.pageY - canvtop
+
+  let name = prompt('What is this location\'s name?')
+  points.push(new Location(name, x, y))
+
+}, false)
 inp.addEventListener('change', handleFiles)
 h1.innerText = 'nope'
+
+
+
+
+
 function handleFiles(e) {
     const data = e.target.files[0]
     let img = new Image()
@@ -18,7 +41,16 @@ function handleFiles(e) {
     img.src = URL.createObjectURL(data)
 }
 
+function updatePointList(loc) {
+    let el = document.createElement('p')
+    el.innerText = loc.toString()
+    pList.appendChild(el)
+}
 
-
-
-
+class Location {
+  constructor(name, x, y) {
+    this.name = name
+    this.x = x
+    this.y = y
+  }
+}
