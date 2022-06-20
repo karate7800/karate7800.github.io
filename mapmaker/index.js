3adbf02f0278974878671a173e0ae006513d9a5c
karate7800 <36754494+karate7800@users.noreply.github.com>
2022-06-19 23:20:07 -0700

    change click event to change event

--- mapmaker/index.js
+++ mapmaker/index.js
@@ -6,9 +6,9 @@ const h1 =document.getElementById('h1')
 const canv = document.getElementById('canvas')
 const ctx = canv.getContext('2d')
  
-inp.addEventListener('click', handleFiles)
+inp.addEventListener('change', handleFiles)
 h1.innerText = 'nope'
-   function handleFiles(e) {
+function handleFiles(e) {
     const data = e.target.files[0]
     let img = new Image()
     img.onload = function() {
